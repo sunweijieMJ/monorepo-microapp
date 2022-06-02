e4050953a3529b6413eb96d34d77c8367f0b290c
Sun, Paul,  Connext China <Paul.Sun1@connext.com.cn>
2022-06-02 14:43:35 +0800

    fix: fix package

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   ],
   plugins: ['stylelint-order'],
   rules: {
-    'max-nesting-depth': 10, // 限制嵌套深度
+    'max-nesting-depth': 15, // 限制嵌套深度
     'scss/dollar-variable-pattern': [/$/, { ignore: 'global' }], // scss 变量名忽略警告
     'selector-class-pattern': '^[a-z][a-zA-Z0-9_-]+$|^el-|^mz-', // 为类选择器指定一个匹配模式
     'declaration-block-trailing-semicolon': null, // 要求或禁止在声明块中使用拖尾分号
@@ -17,16 +17,29 @@ module.exports = {
     'alpha-value-notation': ['number'], // 字母值表示法
     'keyframes-name-pattern': '^[a-zA-Z-]+$', // 动画名称匹配模式
     'custom-property-pattern': '^[a-zA-Z-]+$', // 自定义属性匹配模式
+    'no-descending-specificity': null, // 禁止特异性较低的选择器在特异性较高的选择器之后重写
     'selector-pseudo-element-no-unknown': [
       true,
       { ignorePseudoElements: ['v-deep'] },
     ], // 禁止未知的伪类选择器
     'function-no-unknown': [
-      // 禁用未知的函数
       true,
       {
         ignoreFunctions: ['constant', 'env', 'fade-out', '-'],
       },
-    ],
+    ], // 禁用未知的函数
+    'font-family-no-missing-generic-family-keyword': [
+      true,
+      {
+        ignoreFontFamilies: ['iconfont'],
+      },
+    ], // 不允许在字体系列名称列表中缺少通用系列
+    'value-no-vendor-prefix': [
+      true,
+      {
+        ignoreValues: ['box'],
+      },
+    ], // 禁用值的浏览器前缀
+    'media-feature-name-no-vendor-prefix': null, // 不允许媒体功能名称使用供应商前缀。
   },
 };
